288c400ad577967a3be4ee5041bf3e3988ddf7d0
32-Mimhakkani <mim.cse32@gmail.com>
2022-02-17 11:06:36 +0600

    using dynamic routing

---
+++ pages/Products/[p_id].js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+
+
+const details = () => {
+       const router = useRouter();
+       const detailsid =router.query.p_id
+   details
+
+    return (
+        <h2>
+            dynamic routing the number is : {detailsid}
+        </h2>
+    );
+}
+
+export default details;
---
+++ pages/Products/index.js
@@ -0,0 +1,11 @@
+
+
+const index = () => {
+    return (
+        <h1>
+            Wellcome to our products used for dynamic routing 
+        </h1>
+    );
+};
+
+export default index;

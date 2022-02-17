2600d5c2a4d75708efe920457d1985b3a9826c30
32-Mimhakkani <mim.cse32@gmail.com>
2022-02-17 10:31:09 +0600

    file based routing

---
+++ pages/About.js
@@ -0,0 +1,10 @@
+
+const About = () => {
+    return (
+        <div>
+            <h3>About me to knowing ?? file base roue</h3>
+        </div>
+    );
+};
+
+export default About;
---
+++ pages/profile.js
@@ -0,0 +1,11 @@
+
+
+const profile = () => {
+    return (
+        <div>
+          <h1>   This is the profile page testing the routing</h1>
+        </div>
+    );
+};
+
+export default profile;
